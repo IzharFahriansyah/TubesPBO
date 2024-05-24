060ef2325b205b77b619b59d290da4f24be2799a
izharfahriansyah <izharfahriansyah7@gmail.com>
2024-05-24 21:44:23 +0700

    first commit

---
+++ src/tubespbo/Status.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubespbo;
+
+/**
+ *
+ * @author fauzan
+ */
+public class Status {
+    public Status Progress() {
+        return null;
+    }
+
+    public Status Selesai() {
+        return null;
+    }
+}
---
+++ src/tubespbo/TUBESPBO.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubespbo;
+
+/**
+ *
+ * @author fauzan
+ */
+public class TUBESPBO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Registrasi rg = new Registrasi();
+        rg.setVisible(true);
+        rg.pack();
+        rg.setLocationRelativeTo(null);
+        rg.setDefaultCloseOperation(Registrasi.EXIT_ON_CLOSE);
+    }
+    
+}
